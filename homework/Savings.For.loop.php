d47f1df8ab9152254c9936301712b9b81fed9c58
erikayos <artofkayos@gmail.com>
2015-09-22 21:29:05 -0700

    None

---
+++ homework/Savings.For.loop.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
++    <head>
++    <meta charset="utf-8">
++    <title>Savings For-Loop</title>
++		<?php
++            //Dr. Mark E. Lehr
++            //Date:  Sept 21st, 2015
++            //Purpose:  Logical Operators
++        ?>
++    </head>
++    
++    <body>
++		<?php
++		     //Declare Variables
++			 $nYears=50;  //Number of years in table
++			 $iRate=0.06; //Interest Rate
++			 $prin=100;   //Initial Balance in $'s
++			 //Create headings in the table
++			 echo '<table width="300" border="1">';
++			 echo '<tr><th>Years</th><th>'.($iRate*100).'% Savings</th></tr>';
++			 //Fill the table by using a for-loop row by row
++			 for($year=1;$year<=$nYears;$year++){
++				 $prin*=(1+$iRate);
++				 $prin=number_format($prin,2,'.','');//Limiting to pennies with truncation
++				 echo '<tr>';
++				 	echo "<td>$year</td>";
++					echo '<td>$'.$prin.'</td>';
++				 echo '</tr>';
++			 }
++        	 echo '</table>';
++        ?>
++    </body>
++</html> 
